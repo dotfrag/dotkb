1e9ca9fafc32cf2c6f605bce398fe888060693d9
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-03-12 15:27:25 +0200

    update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,25 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      defaultFlavour: "mocha",
+    }),
+    require("@tailwindcss/typography"),
+    ({ addComponents, theme }) => {
+      addComponents({
+        ".container": {
+          maxWidth: theme("maxWidth.6xl"),
+          marginLeft: theme("margin.auto"),
+          marginRight: theme("margin.auto"),
+          paddingLeft: theme("padding.4"),
+          paddingRight: theme("padding.4"),
+        },
+      });
+    },
+  ],
 };
